6b3c7d22cead770a019f2cf23987227b8bb0366c
Robin Arellano <robinarehen@gmail.com>
2020-04-28 16:38:21 +0200

    refactor: 1. se implementa la consula de la configuración mediante el servicio [config-server].

--- src/main/java/com/rahdevelopers/api/zuulserver/config/OauthResourceServerConfig.java
+++ src/main/java/com/rahdevelopers/api/zuulserver/config/OauthResourceServerConfig.java
@@ -1,5 +1,9 @@
 package com.rahdevelopers.api.zuulserver.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +14,19 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @RefreshScope
 @Configuration
 @EnableResourceServer
 public class OauthResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+	@Value("${config.security.ouath.jwt.key}")
+	private String jwtKey;
+	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(this.tokenStore());
@@ -27,7 +38,31 @@ public class OauthResourceServerConfig extends ResourceServerConfigurerAdapter {
 				.antMatchers(HttpMethod.GET, "/api/producto/listar", "/api/item/listar").permitAll()
 				.antMatchers(HttpMethod.GET, "/api/producto/**", "/api/item/**").hasRole("USER")
 				.antMatchers("/api/producto/**", "/api/item/**").hasRole("ADMIN")
-				.anyRequest().authenticated();
+				.anyRequest().authenticated()
+				.and().cors().configurationSource(corsConfigurationSource());
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-type"));
+		corsConfiguration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+
+		corsSource.registerCorsConfiguration("/**", corsConfiguration);
+
+		return corsSource;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		CorsFilter corsFilter = new CorsFilter(this.corsConfigurationSource());
+		FilterRegistrationBean<CorsFilter> beanCorsFilter = new FilterRegistrationBean<>(corsFilter);
+		return beanCorsFilter;
 	}
 
 	@Bean
@@ -38,7 +73,7 @@ public class OauthResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("abc123");
+		tokenConverter.setSigningKey(this.jwtKey);
 		return tokenConverter;
 	}
 }
